209d4c7f8844738c2b24cad45aa30e1c20be682d
JesusEVL <jesusenriquevillasmilandino@gmail.com>
2025-04-18 23:48:41 -0400

    Porfin funcionan los municipios (Efectivamente chambee)

--- src/municipios/municipios.service.ts
+++ src/municipios/municipios.service.ts
@@ -14,6 +14,8 @@ export class MunicipiosService {
     }
 
     async createMunicipio(municipio: MunicDTO) {
+       console.log(municipio)
+        try {
         await this.prismaService.municipios.create({
             data: {
                 municipio: municipio.municipios,
@@ -22,18 +24,30 @@ export class MunicipiosService {
             }
         })
         return {message: 'Municipio creado exitosamente.'}
+       } catch(error){
+        return {message: 'Error al crear municipio.'+ error}
+       }
     }
 
-    async updateMunicipio(id_municipio: number, municipio: MunicDTO) {
-        await this.prismaService.municipios.update({
-            data: {
-                municipio: municipio.municipios
-            },
-            where: {
+    async updateMunicipio(id_municipio: number, municipio: MunicDTO) 
+    {
+       console.log(municipio)
+        try{
+            await this.prismaService.municipios.update({
+                data: 
+                {
+                    municipio: municipio.municipios
+                },
+            where: 
+            {
                 id_municipio: id_municipio
             }
         })
         return {message: 'Municipio actualizado exitosamente.'}
+            }
+            catch(error){
+                return {message: 'Error al actualizar municipio.'+ error}
+            }
     }
 
     async deleteMunicipio(id_municipio: number) {
