5d377c50caa1b660bc0cafa90aa10a9e5e3419a9
Eduardo282x <eduardokunmixyeah@gmail.com>
2026-08-08 14:01:58 -0400

    feat: add dotenv dependency and update database connection handling

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        const databaseUrl = process.env.DATABASE_URL;
-
-        if (!databaseUrl) {
-            throw new Error(
-                'DATABASE_URL no está definido. Asegúrate de cargar el archivo .env o exportar la variable antes de iniciar la aplicación.'
-            );
-        }
-
-        const adapter = new PrismaPg({
-            connectionString: databaseUrl,
-        });
-        super({ adapter });
-    }
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
 }
